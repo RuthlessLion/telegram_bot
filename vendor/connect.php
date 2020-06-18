a4485b254770d6cab00f9b500c397f655b6737c0
Leo <windowtheworldofgames@gmail.com>
2020-06-18 21:20:04 +0700

    Revert "create project"

--- vendor/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $connect = new mysqli("codercat.ru", "rootya", "********", "telegram_mess");
-    $connect->set_charset("utf8");
-
-    if (!$connect) {
-        die('Error connect to DataBase');
-    }
